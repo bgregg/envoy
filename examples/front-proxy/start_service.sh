c113bb9bf58b9c48662e742dee499c5f4f3c4974
Brian Gregg <brian.n.gregg@gmail.com>
2021-12-11 09:36:09 -0500

    remove service 2 (for clarity) and fix filters

--- examples/front-proxy/start_service.sh
+++ examples/front-proxy/start_service.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 redis-server --protected-mode no &
-python3 /code/foo.py &
+python3 /code/set-redis-keys.py &
 python3 /code/service.py &
 envoy -c /etc/service-envoy.yaml --service-cluster "service${SERVICE_NAME}"
