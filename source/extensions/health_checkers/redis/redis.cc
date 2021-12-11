e82269b4104e0e943980224dcd367787c8339416
Brian Gregg <brian.n.gregg@gmail.com>
2021-12-10 21:54:42 -0500

    std::stoi(value->asString())

--- source/extensions/health_checkers/redis/redis.cc
+++ source/extensions/health_checkers/redis/redis.cc
@@ -129,7 +129,7 @@ void RedisHealthChecker::RedisActiveHealthCheckSession::onResponse(
     std::cout << "Is this a number?" << value->asString() << "  " << isNumber(value->asString()) << "\n";
     std::cout << "Is the second condition true? " << (value->asInteger() < std::stoi(parent_.key_)) << "\n";
     if (isNumber(value->asString()) &&
-        value->asInteger() < std::stoi(parent_.key_)) {
+        std::stoi(value->asString()) < std::stoi(parent_.key_)) {
       handleSuccess();
     } else {
       handleFailure(envoy::data::core::v3::ACTIVE);
