cacd5a4a50dac226b319ff52caf3b90ce1700cd8
Noah Picarelli-Kombert <noah.picarelli-kombert@uconn.edu>
2021-12-10 20:56:10 -0500

    debug

--- source/extensions/health_checkers/redis/redis.cc
+++ source/extensions/health_checkers/redis/redis.cc
@@ -126,7 +126,7 @@ void RedisHealthChecker::RedisActiveHealthCheckSession::onResponse(
     }
     break;
   case Type::Spec:
-    if (isNumber(value->asString()) &&
+    if (value->type() == NetworkFilters::Common::Redis::RespType::Integer &&
         value->asInteger() < std::stoi(parent_.key_)) {
       handleSuccess();
     } else {
